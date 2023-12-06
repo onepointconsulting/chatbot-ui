f0fd0a8f0c3d5354437defe158fa18e4dfb517cd
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-12-06 16:51:28 +0000

    Changed the chat layout.

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -53,33 +53,33 @@ function Code({inline, children, ...props}: CodeProps) {
 
 async function handleCopy(text: string, setCopied: ((value: (((prevState: boolean) => boolean) | boolean)) => void)) {
 
-    // Navigator clipboard api needs a secure context (https)
-    if (navigator.clipboard && window.isSecureContext) {
-      await navigator.clipboard.writeText(text);
+  // Navigator clipboard api needs a secure context (https)
+  if (navigator.clipboard && window.isSecureContext) {
+    await navigator.clipboard.writeText(text);
+    setCopied(true)
+    setTimeout(() => setCopied(false), 5000)
+  } else {
+    // Use the 'out of viewport hidden text area' trick
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Move textarea out of the viewport so it's not visible
+    textArea.style.position = "absolute";
+    textArea.style.left = "-999999px";
+
+    document.body.prepend(textArea);
+    textArea.select();
+
+    try {
+      document.execCommand('copy');
       setCopied(true)
       setTimeout(() => setCopied(false), 5000)
-    } else {
-      // Use the 'out of viewport hidden text area' trick
-      const textArea = document.createElement("textarea");
-      textArea.value = text;
-
-      // Move textarea out of the viewport so it's not visible
-      textArea.style.position = "absolute";
-      textArea.style.left = "-999999px";
-
-      document.body.prepend(textArea);
-      textArea.select();
-
-      try {
-        document.execCommand('copy');
-        setCopied(true)
-        setTimeout(() => setCopied(false), 5000)
-      } catch (error) {
-        console.error(error);
-      } finally {
-        textArea.remove();
-      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      textArea.remove();
     }
+  }
 
 }
 
@@ -110,35 +110,43 @@ export default function Messages({data}: MessagesProps) {
         data.map((message: Message, index: number) => (
           <section key={`message_${index}`}>
             <div className={`chat-message flex flex-row ${message.isUser ? 'bg-white' : ''}`}>
-              <div className="flex flex-col">
-                <span className="text-sm text-gray-500 text-center mt-3 w-20">{message.isUser ? "You" : botName}</span>
-                <span
-                  className="text-xs text-gray-400 text-center mt-1 mb-3 w-20">{message.timestamp.toLocaleTimeString()}</span>
+              <div className="text-sm text-gray-500 text-center mt-3 ml-4 min-w-24 flex-none">
+                <img src={message.isUser ? "/user.png" : "/bot.png"} alt={message.isUser ? "user" : botName}
+                     className="mx-auto h-10 w-10"/>
               </div>
-              <section>
-                <Markdown
-                  className="text-gray-900 mx-3 mt-3 markdown-body"
-                  remarkPlugins={[remarkGfm]}
-                  components={{
-                    ul: ({...props}) => <ul
-                      className="space-y-1 text-gray-500 list-disc dark:text-gray-400 ml-5 mb-3" {...props} />,
-                    ol: ({...props}) => <ol
-                      className="space-y-3 text-gray-500 list-decimal dark:text-gray-400 my-3 mx-4" {...props} />,
-                    p: ({...props}) => <p className="font-sans pb-4 sm:pb-2" {...props} />,
-                    code({...props}) {
-                      // @ts-ignore
-                      return <Code {...props} />;
-                    }
-                  }}
-                >{message.text}</Markdown>
-                {!!message.sources &&
-                  <div className="text-sm text-gray-400 mt-2 px-3">
-                    Sources: <span dangerouslySetInnerHTML={{__html: injectSourceLinks(message.sources)}}/></div>}
-              </section>
+              <div className="mr-5">
+                <div className="flex flex-col ml-3">
+                  <span className="text-sm text-gray-500 mt-3">{message.isUser ? "You" : botName}</span>
+                  <span
+                    className="text-xs text-gray-400">{message.timestamp.toLocaleTimeString()}</span>
+                </div>
+                <div className={`chat-message flex flex-row mt-1 mx-3 ${message.isUser ? 'bg-white' : ''}`}>
+                  <section>
+                    <Markdown
+                      className="text-gray-900 mt-1 markdown-body"
+                      remarkPlugins={[remarkGfm]}
+                      components={{
+                        ul: ({...props}) => <ul
+                          className="space-y-1 text-gray-500 list-disc dark:text-gray-400 ml-5 mb-3" {...props} />,
+                        ol: ({...props}) => <ol
+                          className="space-y-3 text-gray-500 list-decimal dark:text-gray-400 my-3 mx-4" {...props} />,
+                        p: ({...props}) => <p className="font-sans pb-4 sm:pb-2" {...props} />,
+                        code({...props}) {
+                          // @ts-ignore
+                          return <Code {...props} />;
+                        }
+                      }}
+                    >{message.text}</Markdown>
+                    {!!message.sources &&
+                      <div className="text-sm text-gray-400 mt-2">
+                        Sources: <span dangerouslySetInnerHTML={{__html: injectSourceLinks(message.sources)}}/></div>}
+                  </section>
+                </div>
+                {!message.isUser && (
+                  <CopyButton message={message}/>
+                )}
+              </div>
             </div>
-            {!message.isUser && (
-              <CopyButton message={message}/>
-            )}
           </section>
         ))
       }

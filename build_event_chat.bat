affd7a007d85be221d6ed31c957fc88ba574f169
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-12-19 17:01:28 +0000

    Fixed build script which was not including any configuration file.

--- build_event_chat.bat
+++ build_event_chat.bat
@@ -1,5 +1,9 @@
 call yarn run build
+
 cd dist
+mkdir config
+copy ..\config\eventChatConfig.js .\config\
+
 powershell Compress-Archive -Force * ..\chat_build_event_chat.zip
 cd ..
 xcopy dist\* C:\development\playground\agents\event_management_agent\ui /e/s

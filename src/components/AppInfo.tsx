48ec35ea47a6afd28fd63190898375e574c2bbf5
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-12-20 14:13:30 +0000

    Formatted.

--- src/components/AppInfo.tsx
+++ src/components/AppInfo.tsx
@@ -1,43 +1,47 @@
-import {handleMessageDispatch} from './MainChat.tsx';
-import {Socket} from 'socket.io-client';
+import { handleMessageDispatch } from './MainChat.tsx';
+import { Socket } from 'socket.io-client';
 import sendWSMessage from '../lib/websocketClient.ts';
-import {useContext} from 'react';
-import {ChatContext} from '../context/ChatContext.tsx';
-import {Action} from '../context/MessageContext.tsx';
-import {signal} from "@preact/signals-react";
+import { useContext } from 'react';
+import { ChatContext } from '../context/ChatContext.tsx';
+import { Action } from '../context/MessageContext.tsx';
+import { signal } from '@preact/signals-react';
 
 const KEY_EXPAND_APP_INFO = 'expandAppInfo';
 
 const displayInfo = signal(window.localStorage[KEY_EXPAND_APP_INFO] === 'true');
 
 function handleExpandAppInfo() {
-  window.localStorage[KEY_EXPAND_APP_INFO] =
-    !window.localStorage[KEY_EXPAND_APP_INFO] ? 'false' :
-      window.localStorage[KEY_EXPAND_APP_INFO] === 'true' ? 'false' : 'true'
+  window.localStorage[KEY_EXPAND_APP_INFO] = !window.localStorage[
+    KEY_EXPAND_APP_INFO
+  ]
+    ? 'false'
+    : window.localStorage[KEY_EXPAND_APP_INFO] === 'true'
+      ? 'false'
+      : 'true';
   displayInfo.value = window.localStorage[KEY_EXPAND_APP_INFO] === 'true';
 }
 
 function changeAppInfoState(expandAppInfo: boolean) {
-  if(!expandAppInfo) {
-    return false
+  if (!expandAppInfo) {
+    return false;
   }
   return displayInfo.value;
 }
 
 function ListItem({
-                    question,
-                    index,
-                    connected,
-                    dispatch,
-                    socket,
-                  }: {
+  question,
+  index,
+  connected,
+  dispatch,
+  socket,
+}: {
   question: string;
   index: number;
   dispatch: React.Dispatch<Action>;
   connected: boolean;
   socket: React.MutableRefObject<Socket | null>;
 }) {
-  const {exampleQuestions, streaming} = useContext(ChatContext);
+  const { exampleQuestions, streaming } = useContext(ChatContext);
   if (!exampleQuestions) return <></>;
   return (
     <div
@@ -63,25 +67,27 @@ function ListItem({
 }
 
 export default function AppInfo({
-                                  dispatch,
-                                  connected,
-                                  socket,
-                                  expandAppInfo,
-                                }: {
+  dispatch,
+  connected,
+  socket,
+  expandAppInfo,
+}: {
   dispatch: React.Dispatch<Action>;
   connected: boolean;
   socket: React.MutableRefObject<Socket | null>;
   expandAppInfo: boolean;
 }) {
   const shouldExpandAppInfo = changeAppInfoState(expandAppInfo);
-  console.log('shouldExpandAppInfo', shouldExpandAppInfo)
-  const {exampleQuestions, defaultQuestionsPrompt} = useContext(ChatContext);
+  console.log('shouldExpandAppInfo', shouldExpandAppInfo);
+  const { exampleQuestions, defaultQuestionsPrompt } = useContext(ChatContext);
   if (!exampleQuestions || exampleQuestions.length === 0) return <></>;
   return (
     <div className="pr-4 lg:pr-0 h-auto pl-4 md:pl-[3.1rem] lg:pl-[3.6rem] bg-[#339ddf] border-l-4 border-blue-400">
       {/* Expander icon for the prompt question */}
-      <div className="flex items-center justify-between w-full py-2 pl-4 pr-2 cursor-pointer"
-           onClick={handleExpandAppInfo}>
+      <div
+        className="flex items-center justify-between w-full py-2 pl-4 pr-2 cursor-pointer"
+        onClick={handleExpandAppInfo}
+      >
         {/* Default questions prompt */}
         <h1 className="text-xl font-bold text-white md:text-2xl">
           {defaultQuestionsPrompt}
@@ -98,26 +104,24 @@ export default function AppInfo({
           />
         </div>
       </div>
-      {shouldExpandAppInfo && <div
-        className="w-full [&_img]:open:-rotate-180"
-      >
-        {/* Related question items */}
+      {shouldExpandAppInfo && (
+        <div className="w-full [&_img]:open:-rotate-180">
+          {/* Related question items */}
 
-        <div
-          className="w-fit pl-[10px] transition-all  scale-x-105 opacity-100 md:pl-[16px] lg:pl-[22px] xl:pl-[32px] 2xl:pl-8 open:scale-x-0 open:opacity-0">
-          {exampleQuestions?.map((question: string, index: number) => (
-            <ListItem
-              key={`question_${index}`}
-              question={question}
-              index={index}
-              socket={socket}
-              dispatch={dispatch}
-              connected={connected}
-            />
-          ))}
+          <div className="w-fit pl-[10px] transition-all  scale-x-105 opacity-100 md:pl-[16px] lg:pl-[22px] xl:pl-[32px] 2xl:pl-8 open:scale-x-0 open:opacity-0">
+            {exampleQuestions?.map((question: string, index: number) => (
+              <ListItem
+                key={`question_${index}`}
+                question={question}
+                index={index}
+                socket={socket}
+                dispatch={dispatch}
+                connected={connected}
+              />
+            ))}
+          </div>
         </div>
-
-      </div>}
+      )}
     </div>
   );
 }

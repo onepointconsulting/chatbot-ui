e3822e36685874d9235f00b6c5be55f2bf4f16ba
Gil Fernandes <gil.fernandes@onepointltd.com>
2024-01-01 17:43:08 +0000

    Formatting.

--- src/components/buttons/ClearButton.tsx
+++ src/components/buttons/ClearButton.tsx
@@ -6,15 +6,17 @@ function clear() {
 }
 
 export default function ClearButton() {
-  return <button
-    className="flex-none h-10 ml-1 mr-2 my-auto hover:transform rounded-2xl hover:bg-scale-100 hover:duration-200 outline-0"
-    onClick={clear}
-  >
-    <img
-      src="/clear.svg"
-      alt="Clear"
-      title="clear the chat"
-      style={{width: '38px'}}
-    />
-  </button>
+  return (
+    <button
+      className="flex-none h-10 ml-1 mr-2 my-auto hover:transform rounded-2xl hover:bg-scale-100 hover:duration-200 outline-0"
+      onClick={clear}
+    >
+      <img
+        src="/clear.svg"
+        alt="Clear"
+        title="clear the chat"
+        style={{ width: '38px' }}
+      />
+    </button>
+  );
 }
